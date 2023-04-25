7dab960ceb9752e4c6939b819b25d3def002ac55
Héctor Vallvé <hectorvallveg@gmail.com>
2023-04-25 16:43:14 +0200

    Pujada per si acas marxa el llum

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+
+    Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = rigidbody2D.position;
+        position.x = position.x + Time.deltaTime * speed;
+        
+        rigidbody2D.MovePosition(position);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+        
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed;
+        }
+        
+        rigidbody2D.MovePosition(position);
+    }
+
+}
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
-
-
+    
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+    
+    public int health { get { return currentHealth; }}
     int currentHealth;
-
+    
+    bool isInvincible;
+    float invincibleTimer;
+    
     Rigidbody2D rigidbody2d;
-    float horizontal; 
+    float horizontal;
     float vertical;
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
         currentHealth = maxHealth;
-
     }
 
     // Update is called once per frame
@@ -27,9 +31,15 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-     
+        
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
-
+    
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
@@ -39,11 +49,18 @@ public class RubyController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
-
 }
